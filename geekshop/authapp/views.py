34bc7bc699c47e00ed39b75f0ecb9d949a02a9bc
Mishka <7822733@mail.ru>
2021-07-20 13:30:36 +0300

    add HW_8

--- geekshop/authapp/views.py
+++ geekshop/authapp/views.py
@@ -1,76 +1,140 @@
-from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from .forms import ShopUserLoginForm, ShopUserEditForm, ShopUserRegisterForm
 from django.contrib import auth
+from django.http import HttpResponseRedirect
 from django.urls import reverse
-from authapp.forms import ShopUserLoginForm, ShopUserEditForm, ShopUserRegisterForm
+from geekshop.settings import BASE_DIR
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import CreateView, UpdateView
+from .models import ShopUser
+from django.shortcuts import get_object_or_404
 
 
-def login(request):
-    title = 'вход'
+class ShopUserLoginView(LoginView):
+    form_class = ShopUserLoginForm
+    template_name = 'authapp/login.html'
 
-    login_form = ShopUserLoginForm(data=request.POST)
+    def get_context_data(self, **kwargs):
+        context = super(ShopUserLoginView, self).get_context_data(**kwargs)
+        context['login_form'] = self.get_form()
+        context['next'] = self.get_redirect_url()
+        context['title'] = 'вход'
+        return context
 
-    _next = request.GET['next'] if 'next' in request.GET.keys() else ''
+    def get_success_url(self):
+        return reverse('index')
 
-    if request.method == 'POST' and login_form.is_valid():
-        username = request.POST['username']
-        password = request.POST['password']
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
 
-        user = auth.authenticate(username=username, password=password)
-        if user and user.is_active:
-            auth.login(request, user)
-            if 'next' in request.POST.keys():
-                return HttpResponseRedirect(request.POST['next'])
-            else:
-                return HttpResponseRedirect(reverse('index'))
-
-    context = {
-        'title': title,
-        'login_form': login_form,
-        'next': _next,
-    }
-    return render(request, 'authapp/login.html', context)
-
-
-def logout(request):
-    auth.logout(request)
-    return HttpResponseRedirect(reverse('index'))
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                if 'next' in request.POST.keys():
+                    return HttpResponseRedirect(request.POST['next'])
+                else:
+                    return HttpResponseRedirect(reverse('index'))
 
 
-def register(request):
-    title = 'регистрация'
+# def login(request):
+#     title = 'вход'
+#     login_form = ShopUserLoginForm(data=request.POST)
+#     next = request.GET['next'] if 'next' in request.GET.keys() else ''
+#     if request.method == 'POST' and login_form.is_valid():
+#         username = request.POST['username']
+#         password = request.POST['password']
+#
+#         user = auth.authenticate(username=username, password=password)
+#         if user and user.is_active:
+#             auth.login(request, user)
+#             if 'next' in request.POST.keys():
+#                 return HttpResponseRedirect(request.POST['next'])
+#             else:
+#                 return HttpResponseRedirect(reverse('index'))
+#     context = {
+#         'title': title,
+#         'login_form': login_form,
+#         'next': next
+#     }
+#     return render(request, 'authapp/login.html', context)
 
-    if request.method == 'POST':
-        register_form = ShopUserRegisterForm(request.POST, request.FILES)
-
-        if register_form.is_valid():
-            register_form.save()
-            return HttpResponseRedirect(reverse('auth:login'))
-    else:
-        register_form = ShopUserRegisterForm()
-
-    context = {
-        'title': title,
-        'register_form': register_form
-    }
-
-    return render(request, 'authapp/register.html', context)
+class ShopUserLogoutView(LogoutView):
+    def post(self, request, *args, **kwargs):
+        print('++++')
+        auth.logout(request)
+        return HttpResponseRedirect(reverse('index'))
 
 
-def edit(request):
-    title = 'редактирование'
+# def logout(request):
+#     auth.logout(request)
+#     return HttpResponseRedirect(reverse('index'))
+#
 
-    if request.method == 'POST':
-        edit_form = ShopUserEditForm(request.POST, request.FILES, instance=request.user)
-        if edit_form.is_valid():
-            edit_form.save()
-            return HttpResponseRedirect(reverse('auth:edit'))
-    else:
-        edit_form = ShopUserEditForm(instance=request.user)
 
-    context = {
-        'title': title,
-        'edit_form': edit_form
-    }
+class RegisterCreateView(CreateView):
+    form_class = ShopUserRegisterForm
+    template_name = 'authapp/register.html'
 
-    return render(request, 'authapp/edit.html', context)
+    def get_success_url(self):
+        return reverse('auth:login')
+
+    def get_context_data(self, **kwargs):
+        context = super(RegisterCreateView, self).get_context_data()
+        context['register_form'] = self.get_form()
+        context['title'] = 'регистрация'
+        return context
+
+
+# def register(request):
+#     title = 'регистрация'
+#     if request.method == 'POST':
+#         register_form = ShopUserRegisterForm(request.POST, request.FILES)
+#         if register_form.is_valid():
+#             register_form.save()
+#             return HttpResponseRedirect(reverse('auth:login'))
+#     else:
+#         register_form = ShopUserRegisterForm()
+#     context = {
+#         'title': title,
+#         'register_form': register_form
+#     }
+#     return render(request, 'authapp/register.html', context)
+
+class ShopUserEditUpdateView(UpdateView):
+    form_class = ShopUserEditForm
+    model = ShopUser
+    template_name = 'authapp/edit.html'
+
+    def get_object(self, queryset=None):
+        self.object = get_object_or_404(ShopUser, pk=self.request.user.pk)
+        return self.object
+
+    def get_success_url(self):
+        return reverse('auth:edit')
+
+    def get_context_data(self, **kwargs):
+        context = super(ShopUserEditUpdateView, self).get_context_data()
+        context['edit_form'] = self.get_form()
+        context['title'] = 'редактирование'
+        return context
+
+# def edit(request):
+#     title = 'редактирование'
+#
+#     if request.method == 'POST':
+#         edit_form = ShopUserEditForm(request.POST, request.FILES, instance=request.user)
+#         if edit_form.is_valid():
+#             edit_form.save()
+#             return HttpResponseRedirect(reverse('auth:edit'))
+#     else:
+#         edit_form = ShopUserEditForm(instance=request.user)
+#
+#     context = {
+#         'title': title,
+#         'edit_form': edit_form
+#     }
+#
+#     return render(request, 'authapp/edit.html', context)
--- geekshop/basketapp/views.py
+++ geekshop/basketapp/views.py
@@ -1,48 +1,59 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect, JsonResponse
-from basketapp.models import Basket
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect, reverse
 from mainapp.models import Product
-from django.urls import reverse
+from .models import Basket
 from django.contrib.auth.decorators import login_required
 from django.template.loader import render_to_string
-# Create your views here.
+from django.http import JsonResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import DetailView
 
 
-@login_required
-def basket(request):
-    if request.user.is_authenticated:
-        basket = Basket.objects.filter(user=request.user).order_by('product__category')
-        context = {
-            'basket': basket
-        }
-        return render(request, 'basketapp/basket.html', context)
+class BasketDatailView(LoginRequiredMixin, DetailView):
+    model = Basket
+    template_name = 'basketapp/basket.html'
 
-    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    def __init__(self, **kwargs):
+        super().__init__(kwargs)
+        self.object = Basket.objects.filter(user=self.request.user)
 
+    def get_object(self, queryset=None):
+        return self.object
+
+    def get_context_data(self, **kwargs):
+        context = super(BasketDatailView, self).get_context_data()
+        context['basket'] = self.object
+        return context
+
+
+# @login_required
+# def basket(request):
+#     if request.user.is_authenticated:
+#         basket = Basket.objects.filter(user=request.user)
+#         context = {
+#             'basket': basket
+#         }
+#
+#         return render(request, 'basketapp/basket.html', context)
+#     return render(request, 'basketapp/basket.html')
 
 @login_required
 def basket_add(request, pk):
-    if 'login' in request.META.get('HTTP_REFERER'):
-        return HttpResponseRedirect(reverse('products:product', args=[pk]))
-
     product = get_object_or_404(Product, pk=pk)
-
     basket = Basket.objects.filter(user=request.user, product=product).first()
-
     if not basket:
         basket = Basket(user=request.user, product=product)
-
     basket.quantity += 1
     basket.save()
-
-    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    if 'login' in request.META.get('HTTP_REFERER'):
+        return HttpResponseRedirect(reverse('products:product', args=[pk]))
+    else:
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
 @login_required
 def basket_remove(request, pk):
     basket_record = get_object_or_404(Basket, pk=pk)
     basket_record.delete()
-
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
@@ -51,20 +62,14 @@ def basket_edit(request, pk, quantity):
     if request.is_ajax():
         quantity = int(quantity)
         new_basket_item = Basket.objects.get(pk=int(pk))
-
         if quantity > 0:
             new_basket_item.quantity = quantity
             new_basket_item.save()
         else:
             new_basket_item.delete()
-
-        basket = Basket.objects.filter(user=request.user). \
-            order_by('product__category')
-
+        basket = Basket.objects.filter(user=request.user).order_by('product__category')
         context = {
             'basket': basket,
         }
-
         result = render_to_string('basketapp/includes/inc_basket_list.html', context)
-
         return JsonResponse({'result': result})
